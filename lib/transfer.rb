0dcdc3e377a952bb5496b9d507e4c6a01c4b7ca6
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-06-21 13:24:38 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,22 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status 
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver 
+    @amount = amount
+    @status = 'pending'
+  end 
+  
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end 
+  
+  def execute_transaction
+    if valid? 
+  end 
+  
+  def reverse_transfer 
+  end 
+  
 end
