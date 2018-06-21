9117b0433451b3b86c732aa2c5112dcf93d9ff4b
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-06-21 13:26:23 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,21 @@ class Transfer
     if valid? 
   end 
   
-
+def execute_transaction 
++    if both_valid? && status == "pending"
++      if amount < self.sender.balance
++        self.sender.balance -= amount
++        self.receiver.balance += amount
++        self.status = "complete"
++      else
++         self.status = "rejected"
++         "Transaction rejected. Please check your account balance."
++      end
++    else
++       self.status = "rejected"
++       "Transaction rejected. Please check your account balance."
++    end
++  end
   
   def reverse_transfer 
   end 
